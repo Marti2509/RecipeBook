dfb8f0472fe89d728ba7abefd88655b4370c9941
Martin Simov <martisimov259@gmail.com>
2023-07-05 10:28:00 +0300

    Added Toastr Notifications to the App

---
+++ RecipeBook.Common/NotificationMessagesConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeBook.Common
+{
+    public static class NotificationMessagesConstants
+    {
+        public const string ErrorMessage = "ErrorMessage";
+
+        public const string WarningMessage = "WarningMessage";
+
+        public const string InformationMessage = "InformationMessage";
+
+        public const string SuccessMessage = "SuccessMessage";
+    }
+}
