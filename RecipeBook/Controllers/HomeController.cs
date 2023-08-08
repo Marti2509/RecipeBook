585ee9b47deb143055a847164f19531d4452ba52
Marti2509 <martisimov259@gmail.com>
2023-08-08 13:00:22 +0300

    Added custom error pages

--- RecipeBook/Controllers/HomeController.cs
+++ RecipeBook/Controllers/HomeController.cs
@@ -20,9 +20,24 @@ namespace RecipeBook.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error404");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+
+            return View();
         }
     }
 }
