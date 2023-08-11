1a26aae5319d5e98dcf9cb648143d9566b3157be
Marti2509 <martisimov259@gmail.com>
2023-08-12 01:20:28 +0300

    Added Tests for ChefService

---
+++ RecipeBook.Core.Tests/ChefServiceTests.cs
@@ -0,0 +1,102 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Debugger.Contracts.HotReload;
+using RecipeBook.Core.Contracts;
+using RecipeBook.Core.Services;
+using RecipeBook.Data;
+
+using static RecipeBook.Core.Tests.SeedDatabase;
+
+namespace RecipeBook.Core.Tests
+{
+    public class ChefServiceTests
+    {
+        private DbContextOptions<RecipeBookDbContext> dbOptions;
+        private RecipeBookDbContext dbContext;
+
+        private IChefService chefService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            this.dbOptions = new DbContextOptionsBuilder<RecipeBookDbContext>()
+                .UseInMemoryDatabase("RecipeBookInMemory" + Guid.NewGuid().ToString())
+                .Options;
+            this.dbContext = new RecipeBookDbContext(this.dbOptions);
+
+            this.dbContext.Database.EnsureCreated();
+            SeedDb(this.dbContext);
+
+            this.chefService = new ChefService(this.dbContext);
+        }
+
+        [Test]
+        public async Task GetChefIdByUserIdAsyncShouldReturnChefId()
+        {
+            Guid userId = ChefUser.Id;
+
+            Guid chefId = await this.chefService.GetChefIdByUserIdAsync(userId);
+
+            Assert.That(chefId == Chef.Id);
+        }
+
+        [Test]
+        public async Task ChefExistsByUserIdAsyncShouldReturnTrueIfExists()
+        {
+            Guid userId = ChefUser.Id;
+
+            bool result = await this.chefService.ChefExistsByUserIdAsync(userId);
+
+            Assert.That(result);
+        }
+
+        [Test]
+        public async Task ChefExistsByUserIdAsyncShouldReturnFalseIfDoesNotExists()
+        {
+            Guid userId = Guid.NewGuid();
+
+            bool result = await this.chefService.ChefExistsByUserIdAsync(userId);
+
+            Assert.That(result == false);
+        }
+
+        [Test]
+        public async Task ChefExistsByNameAsyncShouldReturnTrueIfExists()
+        {
+            string name = "Peshoo";
+
+            bool result = await this.chefService.ChefExistsByNameAsync(name);
+
+            Assert.That(result);
+        }
+
+        [Test]
+        public async Task ChefExistsByNameAsyncShouldReturnFalseIfDoesNotExists()
+        {
+            string name = "PeshooNE";
+
+            bool result = await this.chefService.ChefExistsByNameAsync(name);
+
+            Assert.That(result == false);
+        }
+
+        [Test]
+        public async Task ChefExistsByPhoneNumberAsyncShouldReturnTrueIfExists()
+        {
+            string phone = "0891234567";
+
+            bool result = await this.chefService.ChefExistsByPhoneNumberAsync(phone);
+
+            Assert.That(result);
+        }
+
+        [Test]
+        public async Task ChefExistsByPhoneNumberAsyncShouldReturnFalseIfDoesNotExists()
+        {
+            string phone = "0000000000";
+
+            bool result = await this.chefService.ChefExistsByPhoneNumberAsync(phone);
+
+            Assert.That(result == false);
+        }
+    }
+}
---
+++ RecipeBook.Core.Tests/SeedDatabase.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework.Internal.Execution;
+using RecipeBook.Data;
+using RecipeBook.Data.Models;
+
+namespace RecipeBook.Core.Tests
+{
+    public static class SeedDatabase
+    {
+        public static ApplicationUser ChefUser;
+        public static ApplicationUser User;
+        public static Chef Chef;
+
+        public static void SeedDb(RecipeBookDbContext dbContext)
+        {
+            ChefUser = new ApplicationUser()
+            {
+                UserName = "Pesho",
+                NormalizedUserName = "PESHO",
+                Email = "pesho@chefs.com",
+                NormalizedEmail = "PESHO@CHEFS.COM",
+                EmailConfirmed = true,
+                PasswordHash = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+                ConcurrencyStamp = "caf271d7-0ba7-4ab1-8d8d-6d0e3711c27d",
+                SecurityStamp = "ca32c787-626e-4234-a4e4-8c94d85a2b1c",
+                TwoFactorEnabled = false,
+                FirstName = "Pesho",
+                LastName = "Petrov",
+                PhoneNumber = "0891234567",
+            };
+
+            User = new ApplicationUser()
+            {
+                UserName = "Mitko",
+                NormalizedUserName = "Mitko",
+                Email = "mitko@users.com",
+                NormalizedEmail = "MITKO@USERS.COM",
+                EmailConfirmed = true,
+                PasswordHash = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+                ConcurrencyStamp = "ac67a9dc-9317-4460-b4cc-d57d75918077",
+                SecurityStamp = "c6a9bcfb-a07c-4072-b997-5bcca90f8166",
+                TwoFactorEnabled = false,
+                FirstName = "Mitko",
+                LastName = "Mitkov",
+                PhoneNumber = "0897654321",
+            };
+
+            Chef = new Chef()
+            {
+                PhoneNumber = "0891234567",
+                User = ChefUser,
+                Name = "Peshoo"
+            };
+
+            dbContext.Users.Add(ChefUser);
+            dbContext.Users.Add(User);
+            dbContext.Chefs.Add(Chef);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
